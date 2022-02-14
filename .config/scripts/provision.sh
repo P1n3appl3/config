25a864a16aba7ab2528433bb3229e9e247579b72
Julia Ryan <juliaryan3.14@gmail.com>
2022-02-14 03:23:11 -0800

    Back to X11

--- .config/scripts/provision.sh
+++ .config/scripts/provision.sh
@@ -1,21 +1,23 @@
 #!/bin/zsh
 
-core=(starship zr-git neovim neovim-symlinks ccache rsync ripgrep fd skim sd exa
-    bat ouch-bin git-delta tokei hexyl dog xh choose-bin rm-improved
+core=(starship zr-git neovim neovim-symlinks rsync ripgrep fd skim sd exa
+    bat ouch git-delta tokei hexyl dog xh choose rm-improved ncdu
     lm_sensors htop bottom bandwhich usbtop procs powertop pacman-cleanup-hook)
-dev=(rustup sccache rust-analyzer cargo-edit clang llvm lld lldb gdb bear
+dev=(rustup rust-analyzer cargo-edit clang llvm lld lldb gdb bear
     python python-black pyright lua stylua-bin lua-language-server bash-language-server)
-desktop=(sway i3status-rust pipewire pipewire-pulse pipewire-alsa udiskie light
+desktop=(i3status-rust pipewire pipewire-pulse pipewire-alsa udiskie light
     kitty firefox google-chrome telegram-desktop discord caprine
     lxappearance materia-gtk-theme papirus-icon-theme
-    dunst rofi rofi-calc rofimoji wtype wl-clipboard slurp grim qt5-wayland qt6-wayland
+    dunst rofi rofi-calc rofimoji
+    i3-wm
+    # sway wtype wl-clipboard slurp grim qt5-wayland qt6-wayland
     nerd-fonts-source-code-pro noto-fonts noto-fonts-cjk noto-fonts-emoji
     zathura zathura-pdf-mupdf pavucontrol pamixer mpc mpd mpdris2 ncmpcpp beets mpv imv
-    openrazer-meta nautilus sushi via-bin calibre ntfs-3g udiskie)
+    openrazer-meta nautilus sushi via-bin calibre ntfs-3g)
 games=(steam lutris osu-lazer-bin slippi-online-appimage dolphin-emu snes9x-gtk
-    minecraft pacmc fabric-installer dwarffortress dwarftherapist xdelta3)
+    minecraft-launcher pacmc fabric-installer dwarffortress dwarftherapist xdelta3)
 tools=(obs-studio ffmpeg sox imagemagick krita inkscape kdenlive
-    lmms blender godot freecad python-solidpython
+    lmms helio-workstation-bin musescore blender godot freecad python-solidpython
     kicad kicad-library pcbdraw gtkwave saleae-logic2 openocd logisim)
 # find an audacity fork
 
@@ -32,7 +34,7 @@ mkdir -p .ssh
 lpass login --trust josephryan3.14@gmail.com
 lpass show --notes ssh_private >.ssh/id_rsa
 lpass show --notes ssh_public >.ssh/id_rsa.pub
-# TODO: replace zprofile with .config/environment.d
+# TODO: replace zprofile with .config/environment.d once supported
 echo OPENWEATHERMAP_API_KEY=$(lpass show --notes openweathermap) \
     >>.zprofile
 # TODO: lastpass with attachment downloading michaelfbryan for gpg
@@ -53,44 +55,24 @@ ln -s desktop .config/sway/current_device
 timedatectl set-ntp true
 timedatectl status
 # timedatectl timesync-status
-hwclock --systohc
+sudo hwclock --systohc
 
 # AUR/pacman
 curl -L $(curl -s https://api.github.com/repos/Morganamilo/paru/releases/latest |
-    rg "browser.*x86_64" | cut -d\" -f4) | tar -axf - paru
+    grep "browser.*x86_64" | cut -d\" -f4) | tar --zstd -xf - paru
 ./paru -S paru-bin rate-mirrors-bin
 rm paru
+sudo chown joseph /etc/pacman.d/mirrorlist
 rate-mirrors --save /etc/pacman.d/mirrorlist arch
 
-paru -S ${core[@]} ${dev[@]} ${desktop[@]} ${games[@]} ${tools[@]}
+paru -S ${core[@]}
+paru -S ${dev[@]}
+paru -S ${desktop[@]}
+paru -S ${games[@]}
+paru -S ${tools[@]}
 
 # Neovim
 git clone --depth=1 https://github.com/savq/paq-nvim.git \
     $HOME/.local/share/nvim/site/pack/paqs/start/paq-nvim
-vi --headless +PaqInstall +PaqList +q
-
-# minecraft
-minecraft_mods=(
-    G1epq3jN # advancement info
-    1IjD5062 # continuity
-    P7dR8mSH # fabric api
-    349239   # carpet
-    Orvt0mRa # indium
-    308892   # litematica
-    gvQqBUqZ # lithium
-    244260   # minihud
-    mOgUt4GM # mod menu
-    296468   # no fog
-    hEOCdOgW # phosphor
-    AANobbMI # sodium
-    297344   # tweakeroo
-    225608   # world edit
-)
-fabric-installer client -dir .minecraft
-pacmc init
-for mod in $minecraft_mods; do
-    pacmc install $mod
-done
-
-#TODO
-# etc host localhost make sure gets set
+vi --headless +PaqInstall +q
+vi --headless +PaqList +q
--- .config/scripts/screenshot.sh
+++ .config/scripts/screenshot.sh
@@ -3,8 +3,15 @@
 set -o pipefail
 
 img_path=~/Images/latest_screenshot.png
-{ if [ $# = 0 ]; then slurp | grim -g- -; else grim -; fi; } |
-    tee $img_path |
-    wl-copy -t image/png &&
+
+if [ $XDG_SESSION_TYPE == "wayland" ]; then
+    { if [ $# = 0 ]; then slurp | grim -g- -; else grim -; fi; } |
+        tee $img_path |
+        wl-copy -t image/png
+else
+    maim -u$1 |
+        tee $img_path |
+        xclip -selection clipboard -t image/png
+fi &&
     cp $img_path ~/Images/screenshots/$(date +%s).png &&
     notify-send -u low 'screenshot taken'
