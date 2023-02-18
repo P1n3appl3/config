9924e380b3c76f3740abbd4edc766e7eda3420d9
Joseph Ryan <josephryan3.14@gmail.com>
2023-02-17 21:51:57 -0800

    Customize zsh

---
+++ .config/zsh/history.zsh
@@ -0,0 +1,8 @@
+HISTFILE="$HOME/.zsh_history"
+HISTSIZE=1000000
+SAVEHIST=1000000
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt share_history
