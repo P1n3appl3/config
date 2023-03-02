20b07287ed5518c23d994ffed0872820d2ccaf44
Joseph Ryan <josephryan3.14@gmail.com>
2023-03-01 16:06:12 -0800

    Zsh terminal integration

--- .config/zsh/keybinds.zsh
+++ .config/zsh/keybinds.zsh
@@ -1,25 +1,23 @@
 bindkey -e
-zle-line-init() { echoti smkx; } && zle -N zle-line-init
-zle-line-finish() { echoti rmkx; } && zle -N zle-line-finish
+
 _zle_load() { autoload -U $1 && zle -N $1; }
 _zle_load up-line-or-beginning-search
 _zle_load down-line-or-beginning-search
 bindkey ${terminfo[kcuu1]} up-line-or-beginning-search   # up
 bindkey ${terminfo[kcud1]} down-line-or-beginning-search # down
 _zle_load edit-command-line
-bindkey '^[[101;6u' edit-command-line        # ctrl+shift+e
-bindkey '^[[3~' delete-char                  # del
-bindkey '^[[3;5~' kill-word                  # ctrl+del
+bindkey '\e[101;6u' edit-command-line        # ctrl+shift+e
+bindkey '\e[3~' delete-char                  # del
+bindkey '\e[3;5~' kill-word                  # ctrl+del
 bindkey '^H' backward-kill-word              # ctrl+backspace
-bindkey '^[[1;5D' backward-word              # ctrl+left
-bindkey '^[[1;5C' emacs-forward-word         # ctrl+right
+bindkey '\e[1;5D' backward-word              # ctrl+left
+bindkey '\e[1;5C' emacs-forward-word         # ctrl+right
 bindkey ${terminfo[khome]} beginning-of-line # home
 bindkey ${terminfo[kend]} end-of-line        # end
-bindkey '^[[Z' reverse-menu-complete         # shift+tab
-bindkey '^R' _atuin_search_widget
+bindkey '\e[Z' reverse-menu-complete         # shift+tab
 bindkey -r '^J' '^T'
 
-# backwards-word should go to the end of the last word
+# make "backwards-word" go to the end of the last word
 zle -N backward-word backward-word-end
 backward-word-end() {
     zle .backward-word && ((CURSOR)) || return
---
+++ .config/zsh/terminal.zsh
@@ -0,0 +1,86 @@
+# This is mostly taken from kitty's shell integration docs. The subset of
+# features used here should work with most reasonably modern terminal emulators:
+# - OSC133 prompt awareness
+# - changing window title to current dir/host/command
+# - changing cursor shape to line in the line editor
+# https://sw.kovidgoyal.net/kitty/shell-integration/#how-it-works
+
+typeset -gi _tty_fd
+{
+    zmodload zsh/system && {
+        { [[ -w $TTY     ]] && sysopen -o cloexec -wu _tty_fd -- $TTY     } ||
+        { [[ -w /dev/tty ]] && sysopen -o cloexec -wu _tty_fd -- /dev/tty }
+    }
+} 2>/dev/null || (( _tty_fd = 1 ))
+
+typeset -gi _prompt_state
+_prompt_precmd() {
+    local -i cmd_status=$?
+    if ! zle; then
+        if (( _prompt_state == 1 )); then
+            print -nu $_tty_fd '\e]133;D;'$cmd_status'\a'
+            (( _prompt_state = 2 ))
+        elif (( _prompt_state == 2 )); then
+            print -nu $_tty_fd '\e]133;D\a'
+        fi
+    fi
+
+    local mark1=$'%{\e]133;A\a%}'
+    if [[ -o prompt_percent ]]; then
+        typeset -g precmd_functions
+        if [[ ${precmd_functions[-1]} == _prompt_precmd ]]; then
+            local mark2=$'%{\e]133;A;k=s\a%}'
+            [[ $PS1 == *$mark1* ]] || PS1=${mark1}${PS1}
+            [[ $PS2 == *$mark2* ]] || PS2=${mark2}${PS2}
+            (( _prompt_state = 2 ))
+        else
+            precmd_functions=(${precmd_functions:#_prompt_precmd} _prompt_precmd)
+            if ! zle; then
+                print -rnu $_tty_fd -- $mark1[3,-3]
+                (( _prompt_state = 2 ))
+            fi
+        fi
+    elif ! zle; then
+        print -rnu $_tty_fd -- $mark1[3,-3]
+        (( _prompt_state = 2 ))
+    fi
+}
+
+_prompt_preexec() {
+    PS1=${PS1//$'%{\e]133;A\a%}'}
+    PS2=${PS2//$'%{\e]133;A;k=s\a%}'}
+    print -nu $_tty_fd '\e]133;C\a'
+    (( _prompt_state = 1 ))
+
+    print -nu $_tty_fd '\e[1 q' # block cursor
+}
+
+# use "application mode" inside the line editor so keybinds don't change
+zle-line-init() {
+    print -nu "$_tty_fd" '\e[5 q' # line cursor
+    echoti smkx
+} && zle -N zle-line-init
+zle-line-finish() { echoti rmkx; } && zle -N zle-line-finish
+
+local is_ssh_session="n"
+if [[ -n "$KITTY_PID" ]]; then
+elif [[ -n "$SSH_TTY" ]]; then
+    is_ssh_session="y"
+elif [[ -n "$(command -v who)" ]]; then
+    [[ "$(command who -m 2> /dev/null)" =~ "\([a-fA-F.:0-9]+\)$" ]] && is_ssh_session="y"
+fi
+
+if [[ "$is_ssh_session" == "y" ]]; then
+    functions[_prompt_precmd]+="
+        print -Prnu $_tty_fd \$'\\e]2;'\"%m: \${(%):-%(4~|…/%3~|%~)}\"\$'\\a'"
+    functions[_prompt_preexec]+="
+        print -Prnu $_tty_fd \$'\\e]2;'\"%m: \${(V)1}\"\$'\\a'"
+else
+    functions[_prompt_precmd]+="
+        print -rnu $_tty_fd \$'\\e]2;'\"\${(%):-%(4~|…/%3~|%~)}\"\$'\\a'"
+    functions[_prompt_preexec]+="
+        print -rnu $_tty_fd \$'\\e]2;'\"\${(V)1}\"\$'\\a'"
+fi
+
+preexec_functions+=(_prompt_preexec)
+precmd_functions+=(_prompt_precmd)
