a2789d71f412a4ee2612a7ac680d0cb2cff1933a
Joseph Ryan <josephryan3.14@gmail.com>
2024-01-21 14:22:10 -0800

    Set up rpxy for Cortana

--- machines/cortana/web.nix
+++ machines/cortana/web.nix
@@ -3,7 +3,7 @@
     allowedTCPPorts = [
       22 69 # ssh
       80 443 # http(s)
-      8080 # testing
+      8080 8443 # testing
       22000 # syncthing
     ];
     allowedUDPPorts = [ 22000 21027 ]; # syncthing discovery
@@ -21,56 +21,84 @@
       extraOptions = [ "-alsologtostderr"];
     };
 
+    rust-rpxy = { enable = true;
+      config = let
+        tls = let acme_dir = "/var/lib/acme/pineapple.computer/"; in {
+          https_redirection = true;
+          tls_cert_path = acme_dir + "fullchain.pem";
+          tls_cert_key_path = acme_dir + "key.pem";
+        };
+        proxy = port: [{ upstream = [{ location = "localhost:" + toString port; }]; }];
+        app = name: port: { inherit tls; server_name = name; reverse_proxy = proxy port; };
+      in {
+        listen_port = 8080;
+        listen_port_tls = 8443;
+        listen_ipv6 = true;
+        apps.localhost = {
+          static    = app        "pineapple.computer" 9000;
+          grafana   = app "status.pineapple.computer" 9001;
+          atuin     = app  "atuin.pineapple.computer" 9002;
+          syncthing = app   "sync.pineapple.computer" 9003;
+          uptime    = app "uptime.pineapple.computer" 9004;
+        };
+      };
+    };
+
+    static-web-server = { enable = true;
+      root = "/media/static";
+      listen = "[::]:9000";
+      configuration.general = { health = true; directory-listing = true; };
+    };
+
     grafana = {
       enable = true; settings = {
         server = {
           http_addr = "127.0.0.1";
-          http_port = 9000;
+          http_port = 9001;
           domain = "traffic.pineapple.computer";
         };
       };
     };
 
-    # TODO: replace with rust-rpxy
-    nginx = {enable = true; recommendedProxySettings = true; };
-    nginx.virtualHosts.${config.services.grafana.settings.server.domain} = {
-      locations."/" = { proxyPass = "http://127.0.0.1:8080"; proxyWebsockets = true; };
-    };
-
-    static-web-server = { enable = true;
-      root = "/media/static"; # port 8787
-      configuration.general = { health = true; directory-listing = true; };
-    };
-
     atuin = { enable = true;
-      path = "/atuin"; # TODO: is this needed
-      port = 9001;
+      # path = "/atuin"; # TODO: is this needed
+      port = 9002;
       openRegistration = true; # TODO: how to add my user statically?
     };
 
     syncthing = let home = "/home/joseph/"; in { enable = true;
       # port 8384 by default, /metrics for prometheus
+      guiAddress = "127.0.0.1:9003";
       user = "myusername";
-      dataDir = home + "Documents";
-      configDir = home + ".config/syncthing";
+      # dataDir = home + "Documents";
+      # configDir = home + ".config/syncthing";
       overrideDevices = true; overrideFolders = true;
       settings = {
         devices = {
-          # "WOPR".id = "TODO";
           # "HAL".id = "TODO";
+            "WOPR".id = "N7B4EPQ-B3PLQIZ-NZIGIEC-CDOIZ3B-MLMUIIO-M5SGMJC-JTVRMB3-SUVIVQI";
+             "clu".id = "KK6IRAU-W7HRIGO-TJL7PNN-DRQCLID-4BBPHPH-IRY5TJY-G372KO6-F527XAB";
           "dragon".id = "5WZIGDB-A5E2YCO-VRHUCI7-6O2GPWN-3J6ONPW-IVBHUE5-VM5JHF2-J2277A2";
-          "clu".id = "KK6IRAU-W7HRIGO-TJL7PNN-DRQCLID-4BBPHPH-IRY5TJY-G372KO6-F527XAB";
         };
         folders = {
           "notes" = {
-            path = "/home/joseph/notes";
-            devices = [ "dragon" "clu" ];
+            path = home + "notes";
+            devices = [ "WOPR" "dragon" "clu" ];
+          };
+          "music" = {
+            path = home + "music/library";
+            devices = [ "WOPR" "dragon" "clu" ];
           };
           # "roms" = { };
-          # "music" = { };
           # "recipes" = { };
+          # "screenshots" = { };
+          # "backgrounds" = { };
         };
       };
+    };
+
+    uptime-kuma = { enable = true;
+      settings.PORT = "9004";
     };
   };
 
--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -7,7 +7,7 @@ img_path="${img_dir}/latest_screenshot.png"
 tmp_path=/tmp/screenshot.png
 linkmove() {
     mv -- "$1" "$2"
-    ln -s -- "$2" "$1"
+    ln -sf -- "$2" "$3"
 }
 
 if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
@@ -17,6 +17,5 @@ else
     maim -u"$1" |
         tee $tmp_path | xclip -selection clipboard -t image/png
 fi &&
-    mv $tmp_path "$img_path" &&
-    linkmove "$img_path" "$img_dir/screenshots/$(date +%s).png" &&
+    linkmove "$tmp_path" "$img_dir/screenshots/$(date +%s).png" "$img_path" &&
     notify-send -u low 'screenshot taken'
