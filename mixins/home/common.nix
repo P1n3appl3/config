ce3315f710f144e27e0bcfa1fd9f3e559f4d8143
Joseph Ryan <josephryan3.14@gmail.com>
2024-01-29 02:49:21 -0800

    Add userChrome

--- mixins/home/common.nix
+++ mixins/home/common.nix
@@ -5,7 +5,7 @@
     # Utils
     fzf ripgrep fd bat eza sd dogdns ouch jq xh rbw hyperfine hexyl choose
     tokei zellij rsync zstd lowcharts trippy page pv datamash ascii numbat
-    pinentry sshping
+    pinentry sshping mdcat
     # System info
     htop bottom bandwhich trippy procs smartmontools duf ncdu du-dust
     # Git
