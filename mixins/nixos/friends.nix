affc2d239f470b35ee15393d1d44ccfd06730d87
Julia Ryan <p1n3appl3@users.noreply.github.com>
2025-02-06 16:04:54 -0800

    y'all are cute

--- mixins/nixos/friends.nix
+++ mixins/nixos/friends.nix
@@ -1,11 +1,12 @@
 { pkgs, inputs, ...}: let
   user = keys: groups: {
     initialPassword = "changethis";
-    extraGroups = [ "wheel" ] ++ groups;
+    extraGroups = [ "cute" ] ++ groups;
     isNormalUser = true;
     openssh.authorizedKeys.keys = keys;
   };
 in {
+  users.groups.cute = {};
   users.users = {
     rahul = ( user [
       inputs.rahul-config.resources.pubKeys.rahul
@@ -32,7 +33,7 @@ in {
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFMNJco+7XH1x91n2rPkOS8Wbq0+Bv6KhWWkacN1y1DR"
     ] [];
     wffl = user [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDr8uWFY5hRTOOeGGUdpROyWVxpkJKp5yJQnivGjY09u"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEvEDTy6VowQbkzi0dYiq99OQ3/ClIzFgA+n1u8giZyO"
     ] [];
     vivian = user [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+8Uu2zJEzZYQ3XhYfuu7FZmEci+Ty9r26Z3L+v6TKV"
@@ -45,6 +46,10 @@ in {
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHi5YnRt1VgK8tt6oSPsKo1X+0gcBXVyvCKXM03/vEh"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFWF1MtDV5HJT+GhD8wrKICyDwQK8ZPQTxZdnsfaqWcs"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHi1EEGRry1aD6uPmdlcRqdiTiIty0JlnfoXeM0qKBC"
+    ] [];
+    jyn = user [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCq58SNpVf4jk1Vs5W7Ur4yap/9vSyuKSIo+ZmBHtMwt0sRxqlpaCm7SgxOcggyItzCRgjJd4X4fi5MNufDD8HB26YPO7fC6VJha+CccBX+y67xjGxWEJW8ERSnyD5T6Pf+g9kWQCov6IJreKS7tn0j/rH4GW9EVB+T4G7T4b6anmSz49L8AII/rYDYCYcKU+JSl6fQGrliZP12NZJCW9T5nOhqTBEQBnIrTuUeD2Jdb39jloQ4Xob3UPld63ciWWIOWPF/TL4B3ILeAAz5+yljtI0H5jXmLNkN2noOitZp+9hdD/w7gpcn6WQeQV9hQJRwNhvx0xXfZsnsqkn1Qrvd"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC51nwBzsuJaYqpIcK/F/wIGdvduedtz49G4WkuXZfgkbJ2J459cEQlCt5QQsDRG62cTeSh8fSDUX3vDsU+NuqwF5nba1vdjsCiw7SEDCFf9V8BGSR86T8FZcEyqSuaH00wCzKfLu6i4kkjG1y9nhUXLIe4EFJIxuD6ELKU7OoiJphleqDRouSuEbwNC3BL3TZRJsr7NfbA4IT/k4hBXFRZG/Aae2X77mpGigv2D3l2b7y/03AKhAjSyULpWsj/5C9ZVuEYePH8E9JsbeEXoLhfQIj2ZXzWIOsjB4oaCwD1GLkTmw7pdvYvNI/koDCKQfC3Xsl+XakmbVNPhEtNYN8skAggo+In2UTJDtejTHfv17YPbJ57S7EM3/IeOaTFRcnJkC/bAgIwSDK7YAOhrI7DpQgHDy5Fbasc98Wg6iD4zkaIZrSsUwIDVDJdNm5TQCouIIYvT7ucqv9XtYLUEyvbr/96gcTIViZec2aiR+Jd2fBr2DlqWRAA4N36OiJn1uc="
     ] [];
     dropbear = user [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC//xJSUcC6/k8IjKpWpWhd1DwUL71IKUOP6InBPekYDFIbsHP+RZq1WkEfwE6Mn9Sa2a6KmgCl5Fb+9IrQd45RXGOzvQSA4uNvBW8fB4/rUNeg+oOucxROV7Hx8CLFhpxvl534zLKSEUD2oMHquueAdXNQotKUOk93R466Fo8oWDXInZJQVc1TAJ9wgS0n0QmoUsyN6weWLc16JjJtBZlCzu4mQLhFpXMIY8MUSOD1Nu0iSOXBmWOfFlW0MOc0gqbFcoXw0Pveh19SO5zo0JPX8n2cR9U0iZrRotd/6Y/Rx5S0CoyvuzWFUIv+PeusC729MnyLjktCxAeWBthhxN2Gs00DaABThmxvF8QZMEGqSVxuvn/ke5YjxT/RLeXn1B4Ct695WENtZo1HhrZmmA+8UuLfLm7Rhcq6hhqKcUr6P/RgjPyU+Q0LhFyjfz3r7VWrd4gANp90mpipVc5KS8I5Of4Joc91NLIlnul7SMmSQ2Sn9ojbYQc1sZMXyTe6ndAu0sTDtGzekP838ISLG/1bstJ365xz+pWact86J10l/MdYNco+xv9B+aaQfPuwNmx59CeHxdeE3EcERRzYdsdoj9iVJZ3bCFUuY+waGXp5eI23ewPag1SmZ9tAiLFJc3V9J/k66OF2Srhw+f2QjF0WST6KAU94h0KAXkNv018CAQ=="
