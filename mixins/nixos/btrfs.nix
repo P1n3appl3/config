49488b53b5b858d8272d4b9431ebb2f3d896d7a8
Julia Ryan <juliaryan3.14@gmail.com>
2026-09-16 13:16:11 -0700

    Fix agenix mounting
    It was trying to read my identity from /home/julia/.ssh and failed
    since that subvolume hadn't been mounted yet, which is why it resolved
    itself after a single activation.

--- mixins/nixos/btrfs.nix
+++ mixins/nixos/btrfs.nix
@@ -5,14 +5,14 @@
       options = [ "subvol=${name}" ] ++ opts;
     };
   in {
-    "/"        = subvol "root" [ "compress=zstd" "user_subvol_rm_allowed" ];
+    "/"        = subvol "root" [ "compress=zstd" ];
     "/nix"     = subvol "nix"  [ "compress=zstd" "noatime" ];
-    "/home"    = subvol "home" [ "compress=zstd" ];
+    "/home"    = subvol "home" [ "compress=zstd" "user_subvol_rm_allowed" ]
+      // { neededForBoot = true; }; # for agenix to read my ssh identity
     "/swap"    = subvol "swap" [ "noatime" ];
     "/var/log" = subvol "log"  [ ];
   };
 
   swapDevices = [{ device = "/swap/swapfile"; }];
-
   services.btrfs.autoScrub = { enable = true; fileSystems = [ "/" ]; };
 }
